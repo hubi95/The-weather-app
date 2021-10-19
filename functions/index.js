d9c63500e7c91af7b142b095b37fc171824f3e15
Hubert Jędrzejczak <hubi.mf@gmail.com>
2021-10-19 18:30:01 +0200

    geo interface
    geolocation added
    initial simple interface added

--- functions/index.js
+++ functions/index.js
@@ -13,12 +13,12 @@ const OWM_API = envVar.owm.api;
 app.post('/', (req, res) => {
     console.log(req.body);
     const loc = req.body;
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${loc.lat}&lon=${loc.lon}&appid=${OWM_API}`;
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${loc.lat}&lon=${loc.lon}&units=metric&lang=${loc.lang}&appid=${OWM_API}`;
 
     axios({
         url: url,
         responseType: 'json'
-    }).then(data => res.json(data.data.current));
+    }).then(data => res.json(data.data));
 });
 
 exports.getWeatherData = functions.https.onRequest(app);
--- public/app.js
+++ public/app.js
@@ -23,11 +23,11 @@ const auth = getAuth(firebaseApp);
 
 const searchElement = document.querySelector('[data-city-search]');
 const searchBox = new google.maps.places.SearchBox(searchElement);
-searchBox.addListener('places_changed', () => {
-    const place = searchBox.getPlaces()[0];
-    if (place == null) return;
-    const lat = place.geometry.location.lat();
-    const lon = place.geometry.location.lng();
+
+const getWeatherData = function (lat, lon) {
+    const latitude = lat
+    const longitude = lon
+    const lang = window.navigator.language;
     const url = 'https://us-central1-weather-app-3a7ac.cloudfunctions.net/getWeatherData';
     fetch(url, {
         method: 'POST',
@@ -36,11 +36,77 @@ searchBox.addListener('places_changed', () => {
             'Accept': 'application/json',
         },
         body: JSON.stringify({
-            lat: lat,
-            lon: lon
+            lat: latitude,
+            lon: longitude,
+            lang: lang
         })
     }).then(res => res.json()).then(data => {
         console.log(data);
-        // setWeatherData(data, place.formatted_address)
+        sessionStorage.setItem('weatherData', JSON.stringify(data));
+        renderWeatherData();
     });
+};
+
+searchBox.addListener('places_changed', () => {
+    const place = searchBox.getPlaces()[0];
+    if (place == null) return;
+    const lat = place.geometry.location.lat();
+    const lon = place.geometry.location.lng();
+    sessionStorage.clear();
+    sessionStorage.setItem('place', JSON.stringify(place.formatted_address));
+    getWeatherData(lat, lon);
 });
+
+document.querySelector(['.geo-button']).addEventListener('click', () => {
+    const successCallback = (position) => {
+        sessionStorage.clear();
+        getWeatherData(position.coords.latitude, position.coords.longitude);
+    };
+
+    const errorCallback = (position) => {
+        alert('Unable to retrieve your location');
+    };
+
+    if (!navigator.geolocation) {
+        alert('Geolocation is not supported by your browser');
+    } else {
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {
+            timeout: 5000
+        });
+    };
+});
+
+const renderWeatherData = () => {
+    const temp = document.querySelector('[data-temperature]');
+    const hum = document.querySelector('[data-humidity]');
+    const press = document.querySelector('[data-pressure]');
+    const wind = document.querySelector('[data-windSpeed]');
+    const prec = document.querySelector('[data-precipitationV]');
+    const visb = document.querySelector('[data-visibility]');
+    const summary = document.querySelector('[data-summary]');
+    const location = document.querySelector('[data-location]');
+
+    const weatherData = JSON.parse(sessionStorage.weatherData);
+    let place;
+    const data = weatherData.current;
+
+    if (sessionStorage.getItem('place') === null) {
+        place = weatherData.timezone;
+    } else {
+        place = JSON.parse(sessionStorage.place);
+    }
+
+    console.log(place);
+
+    // console.log(`${place}, ${data}, ${data.rain['1h']}`);
+
+    temp.textContent = data.temp;
+    hum.textContent = data.humidity;
+    press.textContent = data.pressure;
+    wind.textContent = data.wind_speed;
+    prec.textContent = weatherData.minutely['0'].precipitation;
+    visb.textContent = data.visibility;
+    summary.textContent = data.weather['0'].description;
+    location.textContent = place;
+};
+
